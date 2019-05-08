c1cd03986cc0a68d98c4ef56c9a722917917e37c
Aigerim Dautkulova <aigerim.dautkulova@gmail.com>
2019-05-08 16:19:55 +0200

    Issue #2: Pectoral muscle removal - Orientation of the breast.
    Improving the code and changing the documentation

--- preprocessing/is_right.py
+++ preprocessing/is_right.py
@@ -1,22 +1,21 @@
 import numpy as np
 
 def is_right(image):
+    """
 
-    """Function to define the breast orientation.
+    Parameters
+    ----------
+    image: numpy array
+        Image to work with
 
-    Parameter:
-    	image(numpy array): Image to work with.
+    Returns
+    -------
+    boolean
+        True if the orientation is right, false otherwise
 
-    Returns:
-    	True(boolean): If the orientation is right.
-    	False(boolean): If the orientation is left.
-
-   """
+    """
 
     left = np.mean(np.array_split(np.sum(image, axis=0), 2)[0])
     right = np.mean(np.array_split(np.sum(image, axis=0), 2)[1])
 
-    if right > left:
-        return True
-    else:
-        return False
+    return right > left
