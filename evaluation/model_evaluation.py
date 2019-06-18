Improvement in random forest classifier for speed, fixing bug in normalization

evaluation/model_evaluation.py
@@ -7,7 +7,7 @@ import os
 from evaluation.dice_similarity import dice_similarity
 import progressbar
 from prettytable import PrettyTable
-from classifiers.cascade_random_forests import trainCascadeRandomForestClassifier, applyCascade, get_probs, subdivide_dataset_k
+from classifiers.cascade_random_forests import trainCascadeRandomForestClassifier, trainCascadeRandomForestClassifierFaster, applyCascade, get_probs, subdivide_dataset_k
 from sklearn.model_selection import StratifiedKFold
 import xgboost as xgb
 from sklearn.preprocessing import StandardScaler
@@ -248,7 +248,7 @@ def Kfold_FROC_curve_cascadeRF(folds, FROC_samples, train_dataframe, train_metad
     images_name["Class"] = images_name["File name"].isin(contain_tp).astype(int)
 
     # Create a Cross validation object
-    cv = StratifiedKFold(n_splits=folds)
+    cv = StratifiedKFold(n_splits=folds, shuffle=True)
     k_froc_vals = np.zeros((FROC_samples + 2, 3, folds))
     fold = 0
 
@@ -271,8 +271,8 @@ def Kfold_FROC_curve_cascadeRF(folds, FROC_samples, train_dataframe, train_metad
         # Normalize data
         scaler = StandardScaler()
         scaler.fit(x_train_k)
-        X_train = scaler.transform(x_train_k)
-        X_test = scaler.transform(x_test_k)
+        x_train_k = scaler.transform(x_train_k)
+        x_test_k = scaler.transform(x_test_k)
 
         #########################################################################
         #           Cascade Random Forest
@@ -300,7 +300,7 @@ def Kfold_FROC_curve_cascadeRF(folds, FROC_samples, train_dataframe, train_metad
         parameters = {'n_estimators':np.linspace(ntree_first,ntree_last,ntree_num_elems, dtype=int), 'max_features':np.linspace(mtry_first, mtry_last, mtry_num_elems, dtype=int)}
         st = 0.9
 
-        layers = trainCascadeRandomForestClassifier(d_ntree, d_mtry, NP, parameters, st, training_features_tp, training_features_ntp, fullX, fullY, fullValidX, fullValidY, num_layers_to_test)
+        layers = trainCascadeRandomForestClassifierFaster(d_ntree, d_mtry, NP, parameters, st, training_features_tp, training_features_ntp, fullX, fullY, fullValidX, fullValidY, num_layers_to_test, fold)
         probability = get_probs(layers[:num_layers_to_test], x_test_k)
 
 

run_evaluation.py
@@ -41,25 +41,26 @@ def plot_confusion_matrix(cm, classes, ax,
     ax.set_xlabel('Predicted label')
 
 
-test_data = pd.read_csv(r'../dataset_hog/testing.csv')
-test_metadata = pd.read_csv(r'../dataset_hog/testing_metadata.csv')
-test_metadata = recover_filename(test_metadata)
-train_data = pd.read_csv(r'../dataset_hog/training.csv')
-train_metadata = pd.read_csv(r'../dataset_hog/training_metadata.csv')
-train_metadata = recover_filename(train_metadata)
+#test_data = pd.read_csv(r'../dataset_hog/testing.csv')
+#test_metadata = pd.read_csv(r'../dataset_hog/testing_metadata.csv')
+#test_metadata = recover_filename(test_metadata)
+#train_data = pd.read_csv(r'../dataset_hog/training.csv')
+#train_metadata = pd.read_csv(r'../dataset_hog/training_metadata.csv')
+#train_metadata = recover_filename(train_metadata)
 
-total_data = pd.concat([train_data, test_data])
-total_metadata = pd.concat([train_metadata, test_metadata])
+total_data = pd.read_csv(r'../dataset_hog_with_fp/training.csv')
+total_metadata = pd.read_csv(r'../dataset_hog_with_fp/training_metadata.csv')
+total_metadata = recover_filename(total_metadata)
 total_metadata.shape
 
 # Define the parameters for the Cross Validation
 folds = 10   #@param {type:"number"}
 FROC_samples = 15 #@param {type:"number"}
-train_dataframe = train_data
-train_metadata = train_metadata
+#train_dataframe = train_data
+#train_metadata = train_metadata
 index_max_layer = 4 # Means 5 layers maximum
 
 k_froc_vals = Kfold_FROC_curve_cascadeRF(folds, FROC_samples, total_data, total_metadata, path, index_max_layer)
 np.save('froc_k10_5layers.npy', k_froc_vals)
-k_froc_vals, K_froc_mean, std = plot_k_cv_froc(k_froc_vals)
+plot_FROC(k_froc_vals)
 